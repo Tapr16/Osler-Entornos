ea2be4824d17cea908113f2c3672b9b293e2bd77
Thomas Perez <63517634+Tapr16@users.noreply.github.com>
2026-04-20 22:20:29 -0500

    Delete backend directory

--- backend/src/main/java/com/osler/repository/EspecialidadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.osler.repository;
-
-import com.osler.entity.Especialidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EspecialidadRepository extends JpaRepository<Especialidad, Long> {
-    List<Especialidad> findAllByOrderByNombreAsc();
-}
